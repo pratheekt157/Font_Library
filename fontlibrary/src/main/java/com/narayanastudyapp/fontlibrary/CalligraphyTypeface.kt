2d317e886887308dd30cd8f5460c30bf0e9d0ed4
nineleaps <pratheek.t@nineleaps.com>
2021-04-19 11:27:55 +0530

    Update of CalligraphyTypeface.kt

--- fontlibrary/src/main/java/com/narayanastudyapp/fontlibrary/CalligraphyTypeface.kt
+++ fontlibrary/src/main/java/com/narayanastudyapp/fontlibrary/CalligraphyTypeface.kt
@@ -3,13 +3,8 @@ package com.narayanastudyapp.fontlibrary
 import android.content.Context
 import android.graphics.Typeface
 
-class CalligraphyTypeface {
+class CalligraphyTypeface(private var context: Context?) {
 
-    private var context: Context? = null
-
-    fun calligraphyTypeface(context: Context?) {
-        this.context = context
-    }
 
     fun calligraphyTypeface(font: String): Typeface {
         return Typeface.createFromAsset(
